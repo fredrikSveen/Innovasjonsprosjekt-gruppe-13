81b2d66317e7a1f9c60d2380ae374f7fb96170f3
fredrikSveen <57445753+fredrikSveen@users.noreply.github.com>
2019-11-21 21:44:09 +0100

    Update reciever.ino

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,5 +1,6 @@
 //Laget av gruppe 13 under innovasjonsprosjektet i TTT4255, høsten 2019 ved NTNU.
-//Henter inn diverse biblioteker man trenger å få ulik hardware til å fungere som ønsket. Ved rask ggogling
+//Det ligger inn en del linjer med Serial.print() kommandoer, men disse er kun for testing av systemet, så man kan se hvilke verdier man har fått.
+//Henter inn diverse biblioteker man trenger å få ulik hardware til å fungere som ønsket.
 #include <CapacitiveSensor.h>
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -8,7 +9,7 @@
 //Setter innganger for LCD-displayet.
   LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-
+//Definerer ulike variabler som brukes for tidtakingssekvensen.
   int minutter1 = 73;
   int sekunder1 = 0;
   int s_teller1 = 0;
@@ -22,10 +23,10 @@
   bool holding = 0;
 
 
-RF24 radio(7, 8); // CE, CSN. Tallene forklarer hvilken pinneutgang de skal kobles til.
+RF24 radio(7, 8); // CE, CSN. Tallene forklarer hvilken pinneutgang disse to fra bluetooth-enheten, skal kobles til på Arduinoen.
 const byte address[6] = "00001"; //Hvilken adresse som blåtann-enheten skal kommuisere på.
-CapacitiveSensor cap = CapacitiveSensor(4,2); //Hvilke pinner vi kobler til den kapasitive sensoren.
 
+//En boolsk variabel som brukes for å lagre verdien som sendes via bluetooth.
   bool sensor_state = 0;
 
 void setup() {  
@@ -35,17 +36,17 @@ void setup() {
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
-  //Starter LCD-displayet
+  radio.startListening();//Gjør at bluetooth-enheten kun lytter etter signal og ikke sender ut signal.
+  //Starter LCD-displayet, og definerer ar vi har 16*2 tegn til disposisjon.
   lcd.begin(16,2);
 }
 
 void loop() {
   //Kjører loopen hvis radiosignaler er tilgjenglige.
   if (radio.available()) {
-    radio.read(&sensor_state, sizeof(sensor_state));    //Leser dataen fra blåtann-kommuikasjonen
-    Serial.println(sensor_state);
-    Serial.println("");
+    radio.read(&sensor_state, sizeof(sensor_state));    //Leser dataen fra bluetooth-kommuikasjonen
+    //Serial.println(sensor_state);
+    //Serial.println("");
     //Serial.println(test);
 
     //En logisk løkke som skiller mellom hvem sin tur det er, og når den skal gå videre til neste tur.
@@ -72,6 +73,7 @@ void loop() {
 
 //Hvis det er lag 1 sin tur så skal denne if-setningen telle ned tiden for lag 1 på LCD-displayet.
     if(tur == 0) {
+//LCD-skjermen fungerer slik ar du må tømme den for hver nye loop, også printer du direkte ved kommandoen lcd.print()
       lcd.clear();
       lcd.print("Lag 1: ");
       lcd.print(minutter1);
@@ -91,6 +93,7 @@ void loop() {
         minutter1--;
       }
       
+      //Bytter til linje 2 på lcd-skjermen.
       lcd.setCursor(0,1);
       lcd.print("Lag 2: ");
       lcd.print(minutter2);
